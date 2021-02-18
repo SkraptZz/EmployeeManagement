af29afec7fb756d494dbe6dae1e07821fa603e7a
SkraptZz <36505363+SkraptZz@users.noreply.github.com>
2021-02-18 19:58:26 +0100

    Ep 13 er klaret

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -13,12 +13,6 @@ namespace EmployeeManagement
 {
     public class Startup
     {
-        private IConfiguration _config;
-
-        public Startup(IConfiguration config)
-        {
-            _config = config;
-        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -33,13 +27,15 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseFileServer();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync(_config["MyKey"]);
+                    await context.Response.WriteAsync("Hello World!");
                 });
             });
         }
